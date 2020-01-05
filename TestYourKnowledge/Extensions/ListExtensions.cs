cbd3646f1ac013b5e8b6870f29047af6933c0e59
Bartosz Madej <johnnythemadman@gmail.com>
2020-01-05 23:04:47 +0100

    Sounds and Images - order randomized.

---
+++ TestYourKnowledge/Extensions/ListExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestYourKnowledge.Extensions
+{
+    public static class ListExtensions
+    {
+        private static readonly Random _rng = new Random();
+        public static List<T> Shuffle<T>(this IList<T> list)
+        {
+            return list.OrderBy(x => _rng.Next()).ToList();
+        }
+    }
+}
